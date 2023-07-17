2490c77cfc16d142a109e98fe47813c866f1134a
hasnamannai <hasna.mannaii@gmail.com>
2023-07-17 14:22:45 +0200

    start the frontend

---
+++ user/src/components/Entreprises.jsx
@@ -0,0 +1,11 @@
+import {gql, useQuery} from '@apollo/client' //gql used to make queries
+const GET_ENTREPRISES= gql`
+query getEntreprise{
+    
+}`
+
+export default function Entreprises() {
+  return (
+    <div>Entreprises</div>
+  )
+}
